db869219e59e23def88852209a26a17d7386a1c0
Aapo Leppänen <aapo.leppanen@gmail.com>
2022-01-23 23:19:08 +0200

    fixed typo

--- rps-frontend/src/config/config.js
+++ rps-frontend/src/config/config.js
@@ -3,5 +3,5 @@ export const RECENT_LENGTH = 10;
 // export const WS_LINK = "ws://localhost:8080/live";
 // export const API_BASE_URL = "http://localhost:3001/api"; //dev
 
-export const WS_LINK = "ws://rps-reaktor.herokuapp.com";
+export const WS_LINK = "wss://rps-reaktor.herokuapp.com";
 export const API_BASE_URL = "/api";
